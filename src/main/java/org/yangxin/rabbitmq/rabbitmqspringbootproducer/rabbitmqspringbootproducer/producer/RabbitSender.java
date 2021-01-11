0c4b28cc9250e1d94cfa3509093bc1333dfc0f10
yangxin <1393003255@qq.com>
2021-01-12 00:57:30 +0800

    feat: RabbitMQ与SpringBoot2.0整合实战-2

--- src/main/java/org/yangxin/rabbitmq/rabbitmqspringbootproducer/rabbitmqspringbootproducer/producer/RabbitSender.java
+++ src/main/java/org/yangxin/rabbitmq/rabbitmqspringbootproducer/rabbitmqspringbootproducer/producer/RabbitSender.java
@@ -1,5 +1,7 @@
 package org.yangxin.rabbitmq.rabbitmqspringbootproducer.rabbitmqspringbootproducer.producer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
@@ -14,9 +16,34 @@ import java.util.Map;
  * 1/12/21 12:09 AM
  */
 @Component
+@Slf4j
 public class RabbitSender {
 
     private final RabbitTemplate rabbitTemplate;
+
+//    final RabbitTemplate.ConfirmCallback confirmCallback = (correlationData, ack, cause) -> {
+//        System.err.println("correlationData: " + correlationData);
+//        System.err.println("ack: " + ack);
+//        if(!ack){
+//            System.err.println("异常处理....");
+//        }
+//    };
+
+    /**
+     * 消息入队列的回调
+     */
+    private final RabbitTemplate.ConfirmCallback confirmCallback = (correlationData, ack, cause) -> {
+        log.info("correlationData: [{}], ack: [{}]", correlationData, ack);
+
+        if (!ack) {
+            log.error("异常处理……");
+        }
+    };
+
+    /**
+     * 消息不可达的回调
+     */
+    private final RabbitTemplate.ReturnsCallback returnsCallback = (returned) -> log.info("returned: [{}]", returned);
 
     @Autowired
     public RabbitSender(RabbitTemplate rabbitTemplate) {
@@ -26,6 +53,12 @@ public class RabbitSender {
     public void send(Object message, Map<String, Object> propertiesMap) {
         MessageHeaders messageHeaders = new MessageHeaders(propertiesMap);
         Message<Object> msg = MessageBuilder.createMessage(message, messageHeaders);
-        rabbitTemplate.convertAndSend("exchange-1", "springboot.rabbitmq.producer", msg);
+        // id+时间戳，全局唯一
+        CorrelationData correlationData = new CorrelationData("1234567890");
+
+        rabbitTemplate.setConfirmCallback(confirmCallback);
+        rabbitTemplate.setReturnsCallback(returnsCallback);
+//        rabbitTemplate.convertAndSend("exchange-1", "springboot.rabbitmq.producer", msg, correlationData);
+        rabbitTemplate.convertAndSend("exchange-1", "springboot", msg, correlationData);
     }
 }
---
+++ src/test/java/org/yangxin/rabbitmq/rabbitmqspringbootproducer/rabbitmqspringbootproducer/producer/RabbitSenderTest.java
@@ -0,0 +1,32 @@
+package org.yangxin.rabbitmq.rabbitmqspringbootproducer.rabbitmqspringbootproducer.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yangxin
+ * 1/12/21 12:38 AM
+ */
+@SpringBootTest
+class RabbitSenderTest {
+
+    @Autowired
+    private RabbitSender rabbitSender;
+
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+    @Test
+    void send1() {
+        Map<String, Object> propertiesMap = new HashMap<>();
+        propertiesMap.put("number", "123456");
+        propertiesMap.put("send_time", SIMPLE_DATE_FORMAT.format(new Date()));
+
+        rabbitSender.send("Hello RabbitMQ For Spring Boot!", propertiesMap);
+    }
+}
