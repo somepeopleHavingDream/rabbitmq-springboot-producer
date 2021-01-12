91afc362c5cd5d1e1fa36c83ff9eb3116b7d3cc5
yangxin <1393003255@qq.com>
2021-01-12 15:50:35 +0800

    feat: RabbitMQ与SpringBoot2.0整合实战-4

--- src/main/java/org/yangxin/rabbitmq/rabbitmqspringbootproducer/rabbitmqspringbootproducer/producer/RabbitSender.java
+++ src/main/java/org/yangxin/rabbitmq/rabbitmqspringbootproducer/rabbitmqspringbootproducer/producer/RabbitSender.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
+import org.yangxin.rabbitmq.rabbitmqspringbootproducer.rabbitmqspringbootproducer.entity.Order;
 
 import java.util.Map;
 
@@ -35,6 +36,7 @@ public class RabbitSender {
     private final RabbitTemplate.ConfirmCallback confirmCallback = (correlationData, ack, cause) -> {
         log.info("correlationData: [{}], ack: [{}]", correlationData, ack);
 
+        // 入队失败！
         if (!ack) {
             log.error("异常处理……");
         }
@@ -60,5 +62,15 @@ public class RabbitSender {
         rabbitTemplate.setReturnsCallback(returnsCallback);
 //        rabbitTemplate.convertAndSend("exchange-1", "springboot.rabbitmq.producer", msg, correlationData);
         rabbitTemplate.convertAndSend("exchange-1", "springboot", msg, correlationData);
+    }
+
+    public void sendOrder(Order order) {
+        // id+时间戳，全局唯一
+        CorrelationData correlationData = new CorrelationData("0987654321");
+
+        rabbitTemplate.setConfirmCallback(confirmCallback);
+        rabbitTemplate.setReturnsCallback(returnsCallback);
+        rabbitTemplate.convertAndSend("exchange-2",
+                "springboot.rabbitmq.producer", order, correlationData);
     }
 }
--- src/test/java/org/yangxin/rabbitmq/rabbitmqspringbootproducer/rabbitmqspringbootproducer/producer/RabbitSenderTest.java
+++ src/test/java/org/yangxin/rabbitmq/rabbitmqspringbootproducer/rabbitmqspringbootproducer/producer/RabbitSenderTest.java
@@ -3,6 +3,7 @@ package org.yangxin.rabbitmq.rabbitmqspringbootproducer.rabbitmqspringbootproduc
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.yangxin.rabbitmq.rabbitmqspringbootproducer.rabbitmqspringbootproducer.entity.Order;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,11 +23,17 @@ class RabbitSenderTest {
     private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
     @Test
-    void send1() {
+    void send() {
         Map<String, Object> propertiesMap = new HashMap<>();
         propertiesMap.put("number", "123456");
         propertiesMap.put("send_time", SIMPLE_DATE_FORMAT.format(new Date()));
 
         rabbitSender.send("Hello RabbitMQ For Spring Boot!", propertiesMap);
+    }
+
+    @Test
+    void sendOrder() {
+        Order order = new Order("001", "第一个订单");
+        rabbitSender.sendOrder(order);
     }
 }
